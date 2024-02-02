7f77b0c62cfbadb04875778ec4b718fe873b04e8
Dayana Ismailova <DIs@ikod.kz>
2024-02-02 20:18:30 +0600

    feat: init upload components

---
+++ src/components/Upload/UploadDropbox.vue
@@ -0,0 +1,43 @@
+<script setup lang="ts">
+import { ref } from 'vue';
+
+const emit = defineEmits<{
+  upload: [file: File];
+}>();
+
+const is_dragover = ref(false);
+
+function upload({ dataTransfer }: DragEvent) {
+  is_dragover.value = false;
+  if (!dataTransfer) {
+    return;
+  }
+  const files = [...dataTransfer.files];
+  files.forEach((file) => {
+    if (file.type !== 'audio/mpeg') {
+      return;
+    }
+  });
+  console.log(files);
+  // if (file) {
+  //   emit('upload', file);
+  // }
+}
+</script>
+<template>
+  <div
+    class="w-full px-10 py-20 rounded text-center cursor-pointer border border-dashed border-gray-400 text-gray-400 transition duration-500 hover:text-white hover:bg-green-400 hover:border-green-400 hover:border-solid"
+    :class="{ 'bg-green-400 border-green-400 border-solid': is_dragover }"
+    @drag.prevent.stop=""
+    @dragstart.prevent.stop=""
+    @dragend.prevent.stop="is_dragover = false"
+    @dragover.prevent.stop="is_dragover = true"
+    @dragenter.prevent.stop="is_dragover = true"
+    @dragleave.prevent.stop="is_dragover = false"
+    @drop.prevent.stop="upload($event)"
+  >
+    <h5>Drop your files here</h5>
+  </div>
+</template>
+
+<style scoped></style>
